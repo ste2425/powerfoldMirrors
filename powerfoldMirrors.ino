44ac50ef910d402ed3bf2c914968526b6abd809b
Stephen Cooper <ste.cooper@live.co.uk>
2020-05-06 17:44:11 +0100

    initial working. Need to make to handle ignition off with door locked. It closes mirrors instantly

--- powerfoldMirrors.ino
+++ powerfoldMirrors.ino
@@ -1,14 +1,7 @@
-#include <Event.h>
-#include <Timer.h>
+#include "Timer.h"
 
-#include <Canbus.h>
 #include <defaults.h>
 #include <global.h>
-#include <mcp2515.h>
-#include <mcp2515_defs.h>
-
-unsigned char highSpeed = CANSPEED_500;
-unsigned char lowSpeed = CANSPEED_250;
 
 // states
 bool ignitionActive = false;
@@ -19,83 +12,120 @@ bool mirrorsOpen = false;
 Timer mirrorTransitionTimer;
 int mirrorTransitionEvent;
 
-void setup() {
-  Serial.begin(9600); // For debug use
-  
-  if(Canbus.init(highSpeed))  //Initialise MCP2515 CAN controller at the specified speed
-    Serial.println("CAN Init ok");
-  else
-    Serial.println("Can't init CAN");
-    
-  delay(1000);
+const int openPin = 2;    
+const int closePin = 3;
+const int ignitionPin = 4;
 
+const int openMirrorMotorPin = 7;
+const int closeMirrorMotorPin = 8;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starter");
+    
+  pinMode(openPin, INPUT_PULLUP);
+  pinMode(closePin, INPUT_PULLUP);
+  pinMode(ignitionPin, INPUT_PULLUP);
+  
+  pinMode(openMirrorMotorPin, OUTPUT);
+  pinMode(closeMirrorMotorPin, OUTPUT);
+  
+  digitalWrite(openMirrorMotorPin, HIGH);
+  digitalWrite(closeMirrorMotorPin, HIGH);
 }
 
 void loop() {
-  if (!mcp2515_check_message())
-    return;
-  
-  tCAN message;
-
-  if (!mcp2515_get_message(&message))
-    return;
-
-  updateStatesFromMessage(&message);
+  mirrorTransitionTimer.update();
+  updateStatesFromMessage();
 
   updateMirrors();
-
 }
 
-void updateStatesFromMessage(tCAN *msg) {
+// This needs to read from CAN data
+void updateStatesFromMessage() {
   // do some stuff to check if message means ignition or doors etc
+  int openState = digitalRead(openPin);
+  int closeState = digitalRead(closePin);
+  int ignitionState = digitalRead(ignitionPin);
+
+  if (openState == LOW)
+    doorsLocked = false;
+  else if (closeState == LOW)
+    doorsLocked = true;
+
+  ignitionActive = ignitionState == LOW;
 }
 
 
 void updateMirrors() {
-  if (ignitionActive && !mirrorsOpen) {
+  if (ignitionActive && !mirrorsOpen) {    
+    Serial.println("Opening mirrors as ignition on and and mirrors not open");
     openMirrors();
   } else if (ignitionActive){
+    //Serial.println("Ignition on and mirrors open, returning");
     return;
   } else if (doorsLocked && mirrorsOpen) {
+    Serial.println("doors locked and mirrors open. closing mirrors");
     closeMirrors();
   } else if (!doorsLocked && !mirrorsOpen) {
+    Serial.println("doors open and mirrors closed. opening mirrors");
     openMirrors();
   }
 }
+
+void mirrorTransitionEnd(void *context) {
+  stop();
+}
+
+// This is a hack because i dont know C++ well enough to execute mirrorTransitionEnd directly
+void stop() {
+  mirrorsMoving = false;
+  Serial.println("transition end");
+
+  digitalWrite(openMirrorMotorPin, HIGH);
+  digitalWrite(closeMirrorMotorPin, HIGH);
+}
+
 void openMirrors() {
-  if (mirrorsOpen)
+  if (mirrorsOpen) {
+    Serial.println("mirrors already open or opening");
     return;
+  }
     
   mirrorsOpen = true;
 
-  if (mirrorsMoving)
+  if (mirrorsMoving){
+    Serial.println("mirrors already moving. Stopping");
+    stop();
     mirrorTransitionTimer.stop(mirrorTransitionEvent);
+  }
     
   mirrorsMoving = true;
 
-  // some logic to set correct pins
+  Serial.println("Opening");
+   digitalWrite(closeMirrorMotorPin, LOW);
   
   mirrorTransitionTimer.after(5000, mirrorTransitionEnd, (void*)0);
 }
 
 void closeMirrors() {
-  if (!mirrorsOpen)
+  if (!mirrorsOpen) {
+    Serial.println("mirrors already closed or closing");
     return;
+  }
     
   mirrorsOpen = false;
   
-  if (mirrorsMoving)
+  if (mirrorsMoving){
+    Serial.println("mirrors already moving. Stopping");
+    stop();
     mirrorTransitionTimer.stop(mirrorTransitionEvent);
+  }
     
   mirrorsMoving = true;  
   
-  // some logic to set correct pins
+  Serial.println("Closing");
+  digitalWrite(openMirrorMotorPin, LOW);
   
   mirrorTransitionTimer.after(5000, mirrorTransitionEnd, (void*)0);
-}
-
-void mirrorTransitionEnd(void *context) {
-  mirrorsMoving = false;
-
-  // some logic to stop mirrors moving
 }
